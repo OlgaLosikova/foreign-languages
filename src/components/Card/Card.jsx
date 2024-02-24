08e21825d0c510e74c5339b9f2333db2890e4bb2
OlgaLosikova <techtigra@gmail.com>
2024-02-24 20:11:18 +0300

    stylization

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
 import styles from "./Card.module.css";
 export default function Card() {
-  return <div>Карточка</div>;
+  return <div className={styles.card}>Карточка</div>;
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,14 @@
 import styles from "./Header.module.css";
+
 export default function Header() {
-  return <header>хэдер</header>;
+  return <header className={styles.header}>
+    <div className={styles.logo}>
+    <p className={styles.logoText}>Learn English</p></div>
+    <nav>
+      <ul className={styles.navigation}>
+        <li className={styles.navItem}>Log In</li>
+        <li className={styles.navItem}>Sign Up</li>
+      </ul>
+    </nav>
+  </header>;
 }
