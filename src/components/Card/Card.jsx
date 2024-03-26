7fd11f35ed301857728c7fee978bb3f7f97291aa
OlgaLosikova <techtigra@gmail.com>
2024-03-26 16:10:05 +0300

    translate counter

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,12 @@ import EditButton from "../Buttons/EditButton";
 import { useState, useEffect } from "react";
 import data from "../../words.json";
 
-export default function Card({ cardIndex }) {
+export default function Card({ cardIndex, onClickEditButton }) {
   const [checkTranslation, setCheckTranslation] = useState(false);
 
   const handleCheckTranslation = () => {
     setCheckTranslation(true);
+    onClickEditButton(data[cardIndex].id);
   };
   useEffect(() => {
     setCheckTranslation(false);
@@ -17,8 +18,10 @@ export default function Card({ cardIndex }) {
       <p className={styles.title}>{data[cardIndex].english}</p>
       <p className={styles.transcription}>{data[cardIndex].transcription}</p>
       <div>
-        {checkTranslation ? (<p className={styles.title}>{data[cardIndex].russian}</p>) 
-        : (<EditButton
+        {checkTranslation ? (
+          <p className={styles.title}>{data[cardIndex].russian}</p>
+        ) : (
+          <EditButton
             onClickEditButton={handleCheckTranslation}
             textButton="Проверить"
             color="success"
