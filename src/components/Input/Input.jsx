d768760c53ce37e19c3190c0a24940e76a33e76e
OlgaLosikova <techtigra@gmail.com>
2024-03-29 16:33:06 +0300

    Updates

--- src/components/Input/Input.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Input.module.css";
-export default function Input(props) {
-  const {placeholderText,value}=props;
-  return <input type="text" className={styles.input} placeholder={placeholderText} defaultValue={value}/>;
-}
