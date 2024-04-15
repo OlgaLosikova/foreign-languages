e4b37cff0ff08311049c1471ca402a104c776331
OlgaLosikova <techtigra@gmail.com>
2024-04-15 14:17:21 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ export default function App() {
           addWord,
         }}
       >
-        <Router>
+        <Router >
           <Header onClickEditButton={handleAddRowStart} />
           <main>
             <Routes>
